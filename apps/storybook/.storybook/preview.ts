82f346641c8a12eaea168a6132ec866da62be1f1
Artem Iakovlev <iakovlev.artem.a@gmail.com>
2023-07-31 22:41:53 +0400

    Add base styles

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import "../../../packages/ui/styles/index.scss";
+import "./style.scss";
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +10,17 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+  },
+};
+
+export const globalTypes = {
+  dataThemes: {
+    defaultValue: {
+      list: [
+        { name: "Light", dataTheme: "light", color: "#fff" },
+        { name: "Dark", dataTheme: "dark", color: "#000" },
+      ],
     },
   },
 };
